4d6bb75681b1048573f70364417aae1d71b43deb
pkacp <p.kacprzak.96@gmail.com>
2020-12-21 22:21:19 +0100

    Add test for connector

--- spec/star_wars_api_spec.rb
+++ spec/star_wars_api_spec.rb
@@ -4,7 +4,10 @@ describe StarWarsApi do
       expect(StarWarsApi).to respond_to :get_from_plain_url
     end
 
-    it 'should return object when object present at url' do
+    it 'should return success if object found' do
+      stub_request(:get, "https://swapi.dev/api/people/11")
+          .to_return(body: {name: "Anakin Skywalker", species: []}.to_json)
+      expect(StarWarsApi.get_from_plain_url("https://swapi.dev/api/people/11")).to be_an_instance_of Success
     end
 
     it 'should return NotFound error when object not present at url' do
@@ -14,21 +17,32 @@ describe StarWarsApi do
     end
   end
 
-  # context "species resource" do
-  #   it 'should initialize with species param' do
-  #     expect(StarWarsApi.new('species')).to be_an_instance_of StarWarsApi
-  #   end
-  #
-  #   describe "" do
-  #     before :each do
-  #       @connector = StarWarsApi.new('species')
-  #     end
-  #   end
-  # end
-
+  # TODO Use shared examples for all types of resources
   context "people resource" do
     it 'should initialize with people param' do
       expect(StarWarsApi.new('people')).to be_an_instance_of StarWarsApi
+    end
+
+    describe "#find_by_id" do
+      before :each do
+        @connector = StarWarsApi.new('people')
+      end
+
+      it 'should be present' do
+        expect(@connector).to respond_to :find_by_id
+      end
+
+      it 'should return success when character found' do
+        stub_request(:get, "https://swapi.dev/api/people/11")
+            .to_return(body: {name: "Anakin Skywalker", species: []}.to_json)
+        expect(@connector.find_by_id("11")).to be_an_instance_of Success
+      end
+
+      it 'should return NotFound when none character is present at given id' do
+        stub_request(:get, "https://swapi.dev/api/people/999999")
+            .to_return(body: {detail: "Not found"}.to_json, status: :not_found)
+        expect(@connector.find_by_id("999999")).to be_an_instance_of NotFound
+      end
     end
 
     describe "#find_by_name" do
@@ -84,13 +98,13 @@ describe StarWarsApi do
       it 'should return object when found 1 matching' do
         stub_request(:get, "https://swapi.dev/api/people/?search=Luke Skywalker")
             .to_return(body: res_1.to_json)
-        expect(@connector.find_by_name('Luke Skywalker')).to be_an_instance_of Hash #Character
+        expect(@connector.find_by_name('Luke Skywalker')).to be_an_instance_of Success
       end
 
       it 'should return object when found 1 matching case insensitive' do
         stub_request(:get, "https://swapi.dev/api/people/?search=luke skywalker")
             .to_return(body: res_1.to_json)
-        expect(@connector.find_by_name('luke skywalker')).to be_an_instance_of Hash #Character
+        expect(@connector.find_by_name('luke skywalker')).to be_an_instance_of Success
       end
     end
   end
