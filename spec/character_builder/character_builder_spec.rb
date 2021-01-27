9aadb46bf75c686c4d619ea7999f1b3b3cb09897
pkacp <p.kacprzak.96@gmail.com>
2021-01-27 14:36:16 +0100

    Change in interface character tests

--- spec/character_builder/character_builder_spec.rb
+++ spec/character_builder/character_builder_spec.rb
@@ -7,16 +7,8 @@ shared_examples "a character builder" do
     it { is_expected.to respond_to(:set_hair_color).with("dark").argument }
   end
 
-  before do
+  before :each do
     @character_builder = described_class.new
-  end
-
-  describe "#build" do
-    it "should return Character object" do
-      new_character = @character_builder.build
-
-      expect(new_character).to be_an_instance_of Character
-    end
   end
 
   describe "#set_name" do
@@ -38,6 +30,25 @@ shared_examples "a character builder" do
   describe "#set_hair_color" do
     it 'should return builder object' do
       expect(@character_builder.set_hair_color('blonde')).to be_an_instance_of described_class
+    end
+  end
+
+  describe "#build" do
+    it "should return Character object" do
+      new_character = @character_builder.build
+
+      expect(new_character).to be_an_instance_of Character
+    end
+
+    it 'should return Character with correct attributes' do
+      @character_builder.set_name("name")
+      @character_builder.set_age(22)
+      @character_builder.set_hair_color('black')
+      new_character = @character_builder.build
+
+      expect(new_character.name).to eql "name"
+      expect(new_character.age).to eql 22
+      expect(new_character.hair_color).to eql "black"
     end
   end
 
