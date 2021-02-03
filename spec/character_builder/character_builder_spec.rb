fcf6acf1013e1d6c49091edffcc165c15b33131a
pkacp <p.kacprzak.96@gmail.com>
2021-02-03 13:44:10 +0100

    Refactor builder specs

--- spec/character_builder/character_builder_spec.rb
+++ spec/character_builder/character_builder_spec.rb
@@ -2,9 +2,9 @@ shared_examples "a character builder" do
   describe 'implement methods' do
     subject { described_class.new }
     it { is_expected.to respond_to(:build) }
-    it { is_expected.to respond_to(:set_name).with("name name").argument }
-    it { is_expected.to respond_to(:set_age).with(22).argument }
-    it { is_expected.to respond_to(:set_hair_color).with("dark").argument }
+    it { is_expected.to respond_to(:set_name).with(1).argument }
+    it { is_expected.to respond_to(:set_age).with(1).argument }
+    it { is_expected.to respond_to(:set_hair_color).with(1).argument }
   end
 
   before :each do
@@ -22,8 +22,10 @@ shared_examples "a character builder" do
       expect(@character_builder.set_age(22)).to be_an_instance_of described_class
     end
 
-    it 'should not allow age to be negative' do
-      expect(@character_builder.set_age(-22)).to raise_error ArgumentError
+    xit 'should not allow age to be negative' do
+      # https://softwareengineering.stackexchange.com/a/241319
+      # Builder should not provide validation for simple rules, they should be validated in created object, builder can gather all errors and return them
+      # expect(@character_builder.set_age(-22)).to raise_error ArgumentError
     end
   end
 
@@ -34,21 +36,24 @@ shared_examples "a character builder" do
   end
 
   describe "#build" do
-    it "should return Character object" do
-      new_character = @character_builder.build
-
-      expect(new_character).to be_an_instance_of Character
-    end
-
     it 'should return Character with correct attributes' do
       @character_builder.set_name("name")
       @character_builder.set_age(22)
       @character_builder.set_hair_color('black')
       new_character = @character_builder.build
 
+      expect(new_character).to be_an_instance_of Character
       expect(new_character.name).to eql "name"
       expect(new_character.age).to eql 22
       expect(new_character.hair_color).to eql "black"
+    end
+
+    it 'should return validation errors structure of incorrect attributes' do
+      @character_builder.set_age(-22)
+      validation_errors = @character_builder.build
+
+      expect(validation_errors).to be_an_instance_of ValidationErrors
+      expect(validation_errors).to have_at_least(1).things
     end
   end
 
---
+++ spec/character_builder/jedi_character_builder_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe JediCharacterBuilder do
+
+  it_behaves_like "a character builder"
+
+  before do
+    @jedi_builder = described_class.new
+  end
+
+  describe "#add_padawan" do
+    it 'should return builder object' do
+      character = double("Character")
+      expect(@jedi_builder.add_padawan(character)).to be_an_instance_of described_class
+    end
+  end
+
+end
